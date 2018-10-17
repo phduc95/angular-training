a3c79bc28464fe335445b889c659309ef29422e3
phduc <phduc95@gmail.com>
2018-10-17 14:46:45 +0700

    reactive form and build in validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+ import { ReactiveFormsModule } from '@angular/forms'
+// import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
@@ -13,7 +14,7 @@ import  {MediaItemFormComponent} from './media-item-form/media-item-form.compone
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-media-item-form',
   templateUrl: './media-item-form.component.html',
   styleUrls: ['./media-item-form.component.css']
 })
-export class MediaItemFormComponent implements OnInit {
+export class MediaItemFormComponent{
+  form;
+
+  ngOnInit(){
+    this.form = new FormGroup({
+      medium:new FormControl('Movies'),
+      name:new FormControl('',Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')])),
+      category:new FormControl(''),
+      year:new FormControl(''),
+    });
+  }
 
   onSubmit(mediaItem){
     console.log(mediaItem);
-  }
-  constructor() { }
-
-  ngOnInit() {
   }
 
 }
